56eb8380fd6c31150c4f66ee6530bd20f932ca0b
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-05-16 09:12:47 +0530

    Update leech_handler.py

--- bot/handlers/leech_handler.py
+++ bot/handlers/leech_handler.py
@@ -12,8 +12,8 @@ from re import match as re_match
 from asyncio import sleep as asyncio_sleep
 from os.path import join as os_path_join
 from math import floor
-from pyrogram import Client,  InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.filters import Filters
+from pyrogram import Client
+from pyrogram.filters import Filters, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import Message
 from aria2p.downloads import Download, File
 from bot import LOCAL, STATUS, CONFIG, COMMAND
