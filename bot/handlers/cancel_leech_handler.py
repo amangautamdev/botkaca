e367a2f5523f2ee8d093d994c430e72060cd4f9c
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-05-16 09:18:27 +0530

    Update cancel_leech_handler.py

--- bot/handlers/cancel_leech_handler.py
+++ bot/handlers/cancel_leech_handler.py
@@ -8,13 +8,12 @@ LOGGER = logging.getLogger(__name__)
 # GOAL:
 # create /cancel handler
 
-from pyrogram import Client, CallbackQuery
-from pyrogram.filters import Filters
+from pyrogram import Client, filters, CallbackQuery
 from pyrogram.types import Message
 from bot import LOCAL, CONFIG, STATUS, COMMAND
 from typing import Union
 
-@Client.on_message(Filters.command(COMMAND.CANCEL_LEECH))
+@Client.on_message(filters.command(COMMAND.CANCEL_LEECH))
 async def func(client : Client, data : Union[Message, CallbackQuery]):
     gid = ""
     update_fn = None
@@ -58,6 +57,6 @@ async def func(client : Client, data : Union[Message, CallbackQuery]):
             except:
                 pass
                 
-@Client.on_callback_query(Filters.create(lambda _, query: query.data.startswith(COMMAND.CANCEL_LEECH)))
+@Client.on_callback_query(filters.create(lambda _, query: query.data.startswith(COMMAND.CANCEL_LEECH)))
 async def func2(*args, **kwargs):
     return await func(*args, **kwargs)
